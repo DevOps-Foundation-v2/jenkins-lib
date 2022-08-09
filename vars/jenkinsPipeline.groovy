76e111e36ddb2f8e0a15cc28b730c5a9c5f2caf5
clagosu <clagosu@users.noreply.github.com>
2022-08-08 21:32:58 -0400

    Update jenkinsPipeline.groovy

--- vars/jenkinsPipeline.groovy
+++ vars/jenkinsPipeline.groovy
@@ -12,11 +12,7 @@ pipeline {
          steps {
             figlet 'SOURCE-CONTROL-MANAGEMENT'
             checkout scm // clonacion de codigo en nodo
-            withCredentials([string(credentialsId: 'token-slack', variable: 'TOKENPAT')]) {
-            withEnv(["URL=${env.BUILD_URL}"]) {
-                sh('curl -d "text=SE REPORTA QUE: \n Comienza la ejecucion de pipeline \n $URL" -d "channel=pipelines-devops" -H "Authorization: Bearer $TOKENPAT" -X POST https://slack.com/api/chat.postMessage')
-            }
-          }
+            jenkins()
         }
       }
         
