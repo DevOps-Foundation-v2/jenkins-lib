7bc15bcdc50301ccf13947fb4db4b1b39daffbb1
clagosu <clagosu@users.noreply.github.com>
2022-08-08 21:32:16 -0400

    Update jenkins.groovy

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,3 +1,7 @@
 def slackSend(){
-
+withCredentials([string(credentialsId: 'token-slack', variable: 'TOKENPAT')]) {
+            withEnv(["URL=${env.BUILD_URL}"]) {
+                sh('curl -d "text=SE REPORTA QUE: \n Comienza la ejecucion de pipeline \n $URL" -d "channel=pipelines-devops" -H "Authorization: Bearer $TOKENPAT" -X POST https://slack.com/api/chat.postMessage')
+            }
+          }
 }
